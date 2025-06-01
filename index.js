b0cbf3172b9aeb23232bb9903f76ea5ebd1bc780
Thomas <z9176@zavo.be>
2025-06-01 14:57:26 +0200

    sefse

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+//backround beweging
+const imageContainer = document.querySelector('.image-container');
+function moveBackground(e) {
+  const width = imageContainer.offsetWidth;
+  const height = imageContainer.offsetHeight;
+  const x = e.clientX;
+  const y = e.clientY;
+  const moveX = (x / width) * 12;
+  const moveY = (y / height) * 12;
+  imageContainer.style.backgroundPosition = `${moveX}% ${moveY}%`;
+}
+imageContainer.addEventListener('mousemove', moveBackground);
+
